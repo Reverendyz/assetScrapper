d4589cbe24f89267975c1d6b1c970a01076bfa47
Felipe Moreira dos Santos <felipe_santos@trimble.com>
2022-07-26 18:39:04 -0300

    Adding some features and starting Tkinter

--- main.py
+++ main.py
@@ -1,4 +1,9 @@
+import json
 from objscrapper import ObjectScrapper
+
+import tkinter as tk
+from tkinter import StringVar, ttk
+
 
 URL = "https://statusinvest.com.br/fundos-imobiliarios/recr11"
 
@@ -7,6 +12,25 @@ def main():
     scrape = ObjectScrapper()
 
     scrape.scrape(URL)
+
+def __load_json():
+    FILE = 'teste.json'
+    file = open(FILE, 'w') 
+    return json.loads(file)
+
+
+def open_gui():
+    root = tk.Tk()
+
+    root.geometry("640x420")
+    root.title("Assets control")
+    root.resizable(False, False)
+
+    lf = tk.LabelFrame(root, text='Assets')
+
+    
+
+    root.mainloop()
 
 
 if __name__ == "__main__":
